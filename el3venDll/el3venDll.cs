15ee23276261f6bd47940f9e5b96d9b3d6257227
516365291@qq.com <516365291@qq.com>
2015-10-08 19:33:54 +0800

    a

--- el3venDll/el3venDll.cs
+++ el3venDll/el3venDll.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace el3venDll
 {
-    public class el3venDll
+    public class NetEasyUse
     {
+        public static string GetIP()
+        {
+            try
+            {
+                string strUrl = "http://www.ip138.com/ip2city.asp";     //获得IP的网址
+                Uri uri = new Uri(strUrl);
+                WebRequest webreq = WebRequest.Create(uri);
+                Stream s = webreq.GetResponse().GetResponseStream();
+                StreamReader sr = new StreamReader(s, Encoding.Default);
+                string all = sr.ReadToEnd();         //读取网站返回的数据  格式：您的IP地址是：[x.x.x.x]
+                int i = all.IndexOf("[") + 1;
+                string tempip = all.Substring(i, 15);
+                string ip = tempip.Replace("]", "").Replace(" ", "").Replace("<", "");     //去除杂项找出ip
+                return ip;
+            }
+            catch
+            {
+                return "false";
+            }
+        }
     }
 }
